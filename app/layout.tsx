59348d0724f1add8148fd9f38fa198f076f9abd9
LeonRudi <l.rudi07@icloud.com>
2025-06-09 22:10:50 +0200

    padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="de" suppressHydrationWarning>
-      <body className='py-4'>
+      <body className='p-4'>
          <ThemeProvider
             attribute="class"
             defaultTheme="system"
